6ea56c95570a789aeb1290461570cd2aab1601ba
rars <yopunkrap@hotmail.com>
2015-04-25 19:07:44 -0400

    Search

--- app/src/main/java/com/example/raphael/hacksherbrooke/Search.java
+++ app/src/main/java/com/example/raphael/hacksherbrooke/Search.java
@@ -10,10 +10,15 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-
 import java.util.ArrayList;
 
+import static java.lang.Math.abs;
+
 public class Search extends ActionBarActivity {
+
+    ArrayList<String> parameters = new ArrayList<>();
+    ArrayList<BikeRoad> possiblePaths = new ArrayList<>();
+    ArrayList<CheckBox> CheckBoxes = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,8 +41,7 @@ public class Search extends ActionBarActivity {
         //Fetch the necessary data if the button Submit is prerssed
         if(v.getId() == R.id.Submit){
 
-            ArrayList<String> parameters = new ArrayList();
-            ArrayList<CheckBox> CheckBoxes = new ArrayList<CheckBox>();
+
             RadioGroup TerrainGroup=(RadioGroup)findViewById(R.id.radioGroupTerrain);
             RadioGroup TimeGroup=(RadioGroup)findViewById(R.id.radioGroupTime);
             RadioGroup DenivelationGroup=(RadioGroup)findViewById(R.id.radioGroupDenivelation);
@@ -79,11 +83,91 @@ public class Search extends ActionBarActivity {
 
     public void Evaluate(ArrayList<BikeRoad> bikeRoadsArray){
 
-        
+
+
+
+        for(int i = 0; i< bikeRoadsArray.size();i++) {
+
+            BikeRoad path = bikeRoadsArray.get(i);
+
+            double denivelation = findDenivelation(path);
+
+            //If we select asphalte and it is not
+            if (parameters.get(0).equals("Paved") && !path.pavement.equals("Asphalte")) {
+
+                continue;
+
+            }
+
+            //if we select a short length and the length of the path is long
+            if (path.lenght >= 50 && parameters.get(1).equals("30 to 45 min")) {
+
+                continue;
+
+            }
+
+            //Not 1-1h30 and path between 50 and 500
+            if (path.lenght > 50 && path.lenght <= 500 && !parameters.get(1).equals("1h to 1h30 ")) {
+
+                continue;
+
+            }
+
+
+            if (path.lenght > 500 && path.lenght <= 1000 && !parameters.get(1).equals("2h to 2h30")){
+
+                continue;
+
+            }
+
+            if(path.lenght > 1000 && !parameters.get(1).equals("Whole afternoon")){
+
+                continue;
+
+            }
+
+            if(denivelation < 25 && !parameters.get(2).equals("Flat")){
+
+                continue;
+
+            }
+
+            if(denivelation < 100 && denivelation >= 25 && !parameters.get(2).equals("Hills")){
+
+                continue;
+
+            }
+
+            if(denivelation >= 100 && !parameters.get(2).equals("Big Hills")){
+
+                continue;
+
+            }
+
+            possiblePaths.add(path);
+
+
+        }
 
 
     }
 
+
+    public double findDenivelation(BikeRoad bikeRoadsArray){
+
+        double sum = 0;
+
+        for(int k = 1; k< bikeRoadsArray.geometry.size();k++){
+
+
+            sum += abs(bikeRoadsArray.geometry.get(k).altitude - bikeRoadsArray.geometry.get(k).altitude);
+
+        }
+
+        sum = sum/bikeRoadsArray.lenght;
+
+        return sum;
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
