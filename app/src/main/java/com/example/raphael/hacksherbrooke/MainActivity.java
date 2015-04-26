c59b27d96bc4f4a7fe6455559bd55d1d8c68885a
rars <yopunkrap@hotmail.com>
2015-04-26 05:23:04 -0400

    Main3

--- app/src/main/java/com/example/raphael/hacksherbrooke/MainActivity.java
+++ app/src/main/java/com/example/raphael/hacksherbrooke/MainActivity.java
@@ -4,9 +4,16 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.CheckBox;
+
+import com.example.raphael.hacksherbrooke.parsers.DataObjects.Profile;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -25,6 +32,67 @@ public class MainActivity extends ActionBarActivity {
         return true;
     }
 
+    public void OnCoupleView(View v){
+
+        Profile CoupleProfile = new Profile();
+
+        CoupleProfile.profile = "Couple";
+        CoupleProfile.DenivelationSelected = "Hills";
+        CoupleProfile.RideSelected = "Road Bike";
+        CoupleProfile.TerrainChoosen = "Paved";
+        CoupleProfile.TimeSelected = "Whole afternoon";
+    }
+
+    public  void OnMountainView(View v){
+
+        Profile mountain = new Profile();
+        mountain.profile = "Mountain";
+        mountain.DenivelationSelected = "Big Hills";
+        mountain.TerrainChoosen = "Rocky";
+        mountain.RideSelected = "Mountain Bike";
+        mountain.TimeSelected = "2h to 2h30";
+    }
+
+    public void FunView(View v){
+
+        Profile pro = new Profile();
+        pro.profile = "Fun";
+        pro.DenivelationSelected = "Hills";
+        pro.TerrainChoosen = "Rocky";
+        pro.RideSelected = "Kick Scooter";
+        pro.TimeSelected = "1h to 1h30";
+    }
+
+    public void KidsView(View v){
+
+        Profile pro = new Profile();
+        pro.profile = "Kids";
+        pro.DenivelationSelected = "Flat";
+        pro.TerrainChoosen = "Paved";
+        pro.RideSelected = "Rollerblades";
+        pro.TimeSelected = "30 to 45 min";
+    }
+
+    public  void ExtremeMountain(View v){
+
+        Profile pro = new Profile();
+        pro.profile = "Extreme Mountain";
+        pro.DenivelationSelected = "Extreme";
+        pro.TerrainChoosen = "Rocky";
+        pro.RideSelected = "Mountain Bike";
+        pro.TimeSelected = "2h to 2h30";
+    }
+
+    public void RidingBike(View v){
+
+        Profile pro = new Profile();
+        pro.profile = "Riding";
+        pro.DenivelationSelected = "Flat";
+        pro.TerrainChoosen = "Paved";
+        pro.RideSelected = "Road Bike";
+        pro.TimeSelected = "1h to 1h30";
+
+    }
 
     public void OnButtonPressed(View v){
 
